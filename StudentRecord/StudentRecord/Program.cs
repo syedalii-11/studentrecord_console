87da4844353f0e3d5c57598e9a6a92a20ce276df
syedalii-11 <syedalii1@yahoo.com>
2025-02-12 12:20:17 +0530

    Add files via upload

---
+++ StudentRecord/StudentRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using StudentRecord;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new StudentDbContext())
+        {
+            // Ensure database is created
+            context.Database.EnsureCreated();
+
+            // Add a student
+            var student = new Student { Name = "John Doe", Age = 20 };
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            // Fetch and display student data
+            var students = context.Students.ToList();
+            Console.WriteLine("Student List:");
+            foreach (var s in students)
+            {
+                Console.WriteLine($"ID: {s.Id}, Name: {s.Name}, Age: {s.Age}");
+            }
+        }
+    }
+}
---
+++ StudentRecord/StudentRecord/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentRecord
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=students.db");
+        }
+    }
+}
